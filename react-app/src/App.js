8a8ebecfea919d9c38df6db42de341d6bf0e27e8
Leroy Rosales <leroyrosales@gmail.com>
2021-04-27 09:23:50 -0500

    Adds bootstrap, updates layout of cards

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Scholarship from './components/Scholarship'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App({ scholarships }) {
 
   const scholarshipList = scholarships.map((scholarship) => <Scholarship scholarship={scholarship}/>)
 
   return (
-    <div className="App">
+    <div className="row">
       {scholarshipList}
     </div>
   );
--- react-app/src/components/Scholarship.js
+++ react-app/src/components/Scholarship.js
@@ -3,20 +3,20 @@ import React from 'react'
 export default function Scholarship({ scholarship }) {
     console.log(scholarship)
     return (
-        <div>
-            <ul style={{marginBottom: 12 + 'em'}}>
-                <li><strong>{ scholarship.title }</strong></li>
-                <li>{ scholarship.information }</li>
-                <li>{ scholarship.deadline }</li>
-                <li>{ scholarship.amount }</li>
-                <li>{ scholarship.application_online_or_paper }</li>
-                <li>{ scholarship.essays }</li>
-                <li>{ scholarship.letters_of_recommendation }</li>
-                <li>{ scholarship.no_of_awards }</li>
-                <li>{ scholarship.texas_resident }</li>
-                <li>{ scholarship.transcripts }</li>
-                <li>{ scholarship.website }</li>
-                <li>{ scholarship.year_in_school }</li>
+        <div className="col-6 col-md-4" data-year-in-hs="{ scholarship.year_in_school }">
+            <h3>{ scholarship.title }</h3>
+            <hr/>
+            Award amount: { scholarship.amount }
+            <div dangerouslySetInnerHTML={{ __html: scholarship.information }}></div>
+            <ul>
+                <li>Deadline: { scholarship.deadline }</li>
+                <li>Application (online or paper): { scholarship.application_online_or_paper }</li>
+                <li>Essay required?: { scholarship.essays }</li>
+                <li>Letter of recommendation?: { scholarship.letters_of_recommendation }</li>
+                <li>Number of awards offered: { scholarship.no_of_awards }</li>
+                <li>Requires Texas residency?: { scholarship.texas_resident }</li>
+                <li>Requires transcript?: { scholarship.transcripts }</li>
+                <li><a target="_blank" href="{ scholarship.website }" rel="noindex, nofollow">{ scholarship.website }</a></li>
             </ul>
         </div>
     )
