e034740dd75cc20e7fa6257430485bb7f3aaf37d
Leroy Rosales <leroyrosales@gmail.com>
2021-04-27 10:57:48 -0500

    Adds accordion and layout

--- react-app/src/components/Scholarship.js
+++ react-app/src/components/Scholarship.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
 
 export default function Scholarship({ scholarship }) {
     console.log(scholarship)
@@ -6,18 +8,31 @@ export default function Scholarship({ scholarship }) {
         <div className="col-6 col-md-4" data-year-in-hs="{ scholarship.year_in_school }">
             <h3>{ scholarship.title }</h3>
             <hr/>
-            Award amount: { scholarship.amount }
-            <div dangerouslySetInnerHTML={{ __html: scholarship.information }}></div>
-            <ul>
-                <li>Deadline: { scholarship.deadline }</li>
-                <li>Application (online or paper): { scholarship.application_online_or_paper }</li>
-                <li>Essay required?: { scholarship.essays }</li>
-                <li>Letter of recommendation?: { scholarship.letters_of_recommendation }</li>
-                <li>Number of awards offered: { scholarship.no_of_awards }</li>
-                <li>Requires Texas residency?: { scholarship.texas_resident }</li>
-                <li>Requires transcript?: { scholarship.transcripts }</li>
-                <li><a target="_blank" href="{ scholarship.website }" rel="noindex, nofollow">{ scholarship.website }</a></li>
-            </ul>
+            <p className="mb-3">Award amount: ${ scholarship.amount }</p>
+            <Accordion>
+                <div>
+                    <div>
+                        <Accordion.Toggle className="w-100 text-left" as={Button} variant="link" eventKey="0">
+                            More Info
+                        </Accordion.Toggle>
+                    </div>
+                    <Accordion.Collapse eventKey="0">
+                        <div>
+                            <div dangerouslySetInnerHTML={{ __html: scholarship.information }}></div>
+                            <ul>
+                            <li>Deadline: { scholarship.deadline }</li>
+                            <li>Application (online or paper): { scholarship.application_online_or_paper }</li>
+                            <li>Essay required?: { scholarship.essays }</li>
+                            <li>Letter of recommendation?: { scholarship.letters_of_recommendation }</li>
+                            <li>Number of awards offered: { scholarship.no_of_awards }</li>
+                            <li>Requires Texas residency?: { scholarship.texas_resident }</li>
+                            <li>Requires transcript?: { scholarship.transcripts }</li>
+                            <li>Site: <a target="_blank" href="{ scholarship.website }" rel="noindex, nofollow">{ scholarship.website }</a></li>
+                            </ul>
+                        </div>
+                    </Accordion.Collapse>
+                </div>
+            </Accordion>
         </div>
     )
 }
