46f818fd5eb21127755f62218c42de006b4c04ff
Leroy Rosales <leroyrosales@gmail.com>
2021-04-27 12:48:47 -0500

    Toggle visibility of card data

--- react-app/src/components/Scholarship.js
+++ react-app/src/components/Scholarship.js
@@ -8,7 +8,9 @@ export default function Scholarship({ scholarship }) {
         <div className="col-6 col-md-4" data-year-in-hs="{ scholarship.year_in_school }">
             <h3>{ scholarship.title }</h3>
             <hr/>
-            <p className="mb-3">Award amount: ${ scholarship.amount }</p>
+            { scholarship.amount ?
+                <p className="mb-3">Award amount: ${ scholarship.amount }</p> : null
+            }
             <Accordion>
                 <div>
                     <div>
@@ -20,13 +22,27 @@ export default function Scholarship({ scholarship }) {
                         <div>
                             <div dangerouslySetInnerHTML={{ __html: scholarship.information }}></div>
                             <ul>
-                            <li>Deadline: { scholarship.deadline }</li>
-                            <li>Application (online or paper): { scholarship.application_online_or_paper }</li>
-                            <li>Essay required?: { scholarship.essays }</li>
-                            <li>Letter of recommendation?: { scholarship.letters_of_recommendation }</li>
-                            <li>Number of awards offered: { scholarship.no_of_awards }</li>
-                            <li>Requires Texas residency?: { scholarship.texas_resident }</li>
-                            <li>Requires transcript?: { scholarship.transcripts }</li>
+                            { scholarship.deadline ?
+                                <li>Deadline: { scholarship.deadline }</li> : null
+                            }
+                            { scholarship.application_online_or_paper ?
+                                <li>Application (online or paper): { scholarship.application_online_or_paper }</li> : null
+                            }
+                            { scholarship.essays ?
+                                <li>Essay required?: { scholarship.essays }</li> : null
+                            }
+                            { scholarship.letters_of_recommendation ?
+                                <li>Letter of recommendation?: { scholarship.letters_of_recommendation }</li> : null
+                            }
+                            { scholarship.no_of_awards ?
+                                <li>Number of awards offered: { scholarship.no_of_awards }</li> : null
+                            }
+                            { scholarship.texas_resident ?
+                                <li>Requires Texas residency?: { scholarship.texas_resident }</li> : null
+                            }
+                            { scholarship.transcripts ?
+                                <li>Requires transcript?: { scholarship.transcripts }</li> : null
+                            }
                             <li>Site: <a target="_blank" href="{ scholarship.website }" rel="noindex, nofollow">{ scholarship.website }</a></li>
                             </ul>
                         </div>
