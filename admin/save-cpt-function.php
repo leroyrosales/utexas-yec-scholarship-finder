97a219bde073ce856d42d89ef01837a23cc487a2
Leroy Rosales <leroyrosales@gmail.com>
2021-06-10 15:21:42 -0500

    Updates action to fix postmeta bug, optimize query

--- admin/save-cpt-function.php
+++ admin/save-cpt-function.php
@@ -8,12 +8,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Adds Custom Save Post Function
-add_action( 'save_post_utexas_scholarships', function() {
+add_action( 'updated_post_meta', function(){
+    global $post;
 
     $args = array(
         'post_type' => 'utexas_scholarships',
         'post_status' => 'publish',
         'posts_per_page' => -1,
+        'no_found_rows' => true,
+        'update_post_term_cache' => false,
     );
 
     $query = new WP_Query($args);
@@ -23,27 +26,28 @@ add_action( 'save_post_utexas_scholarships', function() {
         $posts[] = apply_filters(
             'utexas_yec_scholarship_fields',
             array(
-                'title' => html_entity_decode(get_the_title($post->ID),ENT_QUOTES,'UTF-8'),
-                'information' => get_field('information'),
-                'deadline' => get_field('deadline'),
-                'amount' => get_field('amount'),
-                'no_of_awards' => get_field('no_of_awards'),
-                'website' => get_field('website'),
-                'application_online_or_paper' => get_field('application_online_or_paper'),
-                'essays' => get_field('essays'),
-                'letters_of_recommendation' => get_field('letters_of_recommendation'),
-                'transcripts' => get_field('transcripts'),
-                'texas_resident' => get_field('texas_resident'),
-                'year_in_school' => get_field('year_in_school'),
-                'additional_info' => get_field('additional_info'),
-                'stem' => get_field('stem'),
-                'keywords' => get_field('keywords'),
+                'title' => html_entity_decode(get_the_title(),ENT_QUOTES,'UTF-8'),
+                'information' => get_post_meta( $post->ID, 'information'),
+                'deadline' => get_post_meta( $post->ID, 'deadline'),
+                'amount' => get_post_meta( $post->ID, 'amount'),
+                'no_of_awards' => get_post_meta( $post->ID, 'no_of_awards'),
+                'website' => get_post_meta( $post->ID, 'website'),
+                'application_online_or_paper' => get_post_meta( $post->ID, 'application_online_or_paper'),
+                'essays' => get_post_meta( $post->ID, 'essays'),
+                'letters_of_recommendation' => get_post_meta( $post->ID, 'letters_of_recommendation'),
+                'transcripts' => get_post_meta( $post->ID, 'transcripts'),
+                'texas_resident' => get_post_meta( $post->ID, 'texas_resident'),
+                'year_in_school' => get_post_meta( $post->ID, 'year_in_school'),
+                'additional_info' => get_post_meta( $post->ID, 'additional_info'),
+                'stem' => get_post_meta( $post->ID, 'stem'),
+                'keywords' => get_post_meta( $post->ID, 'keywords'),
             )
         );
 
     endwhile;
 
     wp_reset_query();
+
     $data = json_encode($posts);
 
     if (!is_dir(ABSPATH . '/wp-content/plugins/utexas-yec-scholarship-finder/react-app/build/data/')) {
@@ -54,6 +58,7 @@ add_action( 'save_post_utexas_scholarships', function() {
     // Creates a scholarships json file in the uploads folder
     file_put_contents( ABSPATH . '/wp-content/plugins/utexas-yec-scholarship-finder/react-app/build/data/data.json', $data);
 
-});
+}, 5 );
+
 
 
